0008f8b2c98de64f86c4d2c38c14c1362961bca1
Morton Potash <leshnik@users.noreply.github.com>
2021-05-22 06:56:53 +0200

    installer fichiers pour experimenter js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+if(3 == 3){console.log("3 = 3")};
+console.log("Ouvrir console pour voir les messages.")
+console.log("Il faut rafraichir la page dans le navigateur pour voir les changements.")
+let oranges = 5;
+let pommes = 3;
+let ananas = 1;
+let fruit;
+fruit = oranges + pommes + ananas;
+console.log(`Il nous reste ${oranges + pommes + ananas} fruits.`);
+console.log(`Il nous reste ${fruit} fruits.`)
